27584e6ae6f9ac934b5fa2b43671de9b38b5782c
altis10 <altis10@yahoo.com>
2013-12-11 15:09:35 +0200

    Adapter-Facade-1

---
+++ Adapter-Facade/Adapter-Facade/Adapter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Adapter_Facade
+{
+	public class AdapterLargeAnimalToBugs : BugsShowCar
+	{
+		LargeAnimalShowCar LACar;
+
+		public AdapterLargeAnimalToBugs(LargeAnimalShowCar LACar)
+		{
+			this.LACar = LACar;
+		}
+
+		public void SetProtection()
+		{
+			string operation = "Raise protective vail!";
+			LACar.SetProtection();
+			MessageBox.Show(operation);
+		}
+	}
+
+	public class ZooShowCar : ICarInterface
+	{
+		public void GetVisitor()
+		{ 
+
+		}
+
+		public void SetProtection()
+		{
+			string operation = "Set tourist protection!";
+			MessageBox.Show(operation);
+		}
+
+		public void SecureDoors()
+		{
+			string operation = "Doors secured.";
+			MessageBox.Show(operation);
+		}
+
+		public void StartEngine()
+		{
+			string operation = "Engine start.";
+			MessageBox.Show(operation);
+		}
+
+		public void OpenCommChannel()
+		{
+			string operation = "Comm channel opened.";
+			MessageBox.Show(operation);
+		}
+
+		public void SelectDestinationArea()
+		{
+			string operation = "Destination area selected..";
+			MessageBox.Show(operation);
+		}
+
+		public void WalkVisitorThroughLandscape()
+		{
+			string operation = "Walking visitors...";
+			MessageBox.Show(operation);
+		}
+	}
+
+	public class LargeAnimalShowCar : ZooShowCar
+	{
+		public void SetProtection()
+		{
+			string operation = "Raise Bars!"; 
+			MessageBox.Show(operation);
+		}
+	}
+
+	public class FowlShowCar : ZooShowCar
+	{
+		public void SetProtection()
+		{
+			string operation = "Raise protective roof!";
+			MessageBox.Show(operation);
+		}
+	}
+
+	public class BugsShowCar : ZooShowCar
+	{
+		public void SetProtection()
+		{
+			string operation = "Raise protective vail!";
+			MessageBox.Show(operation);
+		}
+	}
+
+	public interface ICarInterface
+	{
+		void GetVisitor();
+		void SetProtection();
+	}
+}
---
+++ Adapter-Facade/Adapter-Facade/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter_Facade
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int visitors;
+
+			visitors = 3; //Convert.ToInt32(args[0]);
+			for (int count = 0; count < visitors - 1; count++)
+			{
+				if (count >= 1)
+				{
+					LargeAnimalShowCar newLACar = new LargeAnimalShowCar();
+					BugsShowCar adaptedCar = new AdapterLargeAnimalToBugs(newLACar);
+					adaptedCar.GetVisitor();
+					adaptedCar.SecureDoors();
+					adaptedCar.SetProtection();
+					//adaptedCar.OpenCommChannel();
+					//adaptedCar.SelectDestinationArea();
+					//adaptedCar.StartEngine();
+					//adaptedCar.WalkVisitorThroughLandscape();
+				}
+				else
+				{
+					BugsShowCar bugcar = new BugsShowCar();
+					bugcar.GetVisitor();
+					bugcar.SecureDoors();
+					bugcar.SetProtection();
+					//bugcar.OpenCommChannel();
+					//bugcar.SelectDestinationArea();
+					//bugcar.StartEngine();
+					//bugcar.WalkVisitorThroughLandscape();
+				}
+			}
+		}
+	}
+}
